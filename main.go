0449156b327cab4f2683eaf1f509a1258a791852
jludvik <jiri.ludvik@techfides.cz>
2025-02-01 22:09:33 +0100

    basic gdrive reading

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"google.golang.org/api/drive/v2"
+	"google.golang.org/api/option"
+	"log"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+const (
+	credentialsFileName = "service-account.json"
+	romFolderID         = "1i-SG3ixBSt5dZPn3ihSD9BsWnzgWUfJm"
+	destionationDir     = "C:\\testDir"
+)
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	ctx := context.Background()
+	srv, err := drive.NewService(ctx, option.WithCredentialsFile(credentialsFileName))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+	fileList, err := srv.Files.List().Q("'" + romFolderID + "' in parents").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+
+	for _, file := range fileList.Items {
+		fmt.Printf("Found file %s \n", file.OriginalFilename)
 	}
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
