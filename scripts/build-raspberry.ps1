de4d0c06ef96b696fc4d018cf0f147da3a7ee7e4
jludvik <jiri.ludvik@techfides.cz>
2025-02-03 08:46:35 +0100

    added build script for raspberry, extended gitignore

---
+++ scripts/build-raspberry.ps1
@@ -0,0 +1,7 @@
+    $rootDir = Join-Path -Path $PSScriptRoot -ChildPath ".."
+    Set-Location $rootDir
+    $env:GOOS="linux";
+    $env:GOARCH="arm";
+    $env:GOARM=7;
+    $env:CGO_ENABLED="0";
+    go build -o bin/rom-downloader
