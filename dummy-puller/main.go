10ea19cec10ced9f9eb9457f938b7cb31bd64f7d
jludvik <jiri.ludvik@techfides.cz>
2025-03-08 17:51:42 +0100

    added dummy puller func

---
+++ dummy-puller/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Variables struct {
+	ProjectID       string
+	SubscriptionID  string
+	maxNackAttempts int
+	messageAgeLimit time.Duration
+}
+
+func main() {
+	ctx := context.Background()
+	variables, err := getEnvVariables()
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := pubsub.NewClient(ctx, variables.ProjectID)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	sub := client.Subscription(variables.SubscriptionID)
+	fmt.Printf("Listening for messages on subscription %s...\n", variables.SubscriptionID)
+
+	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		msgAge := time.Since(msg.PublishTime)
+		fmt.Printf("Received message: %s | Age: %v\n", msg.ID, msgAge)
+
+		if msgAge > variables.messageAgeLimit {
+			fmt.Printf("Message %s is older than %f minutes. Forcing Dead Letter...\n", msg.ID, variables.messageAgeLimit.Minutes())
+			for i := 0; i < variables.maxNackAttempts; i++ {
+				msg.Nack()
+			}
+			fmt.Printf("Message %s has been nacked %d forced to Dead Letter.\n", msg.ID, variables.maxNackAttempts)
+			return
+		}
+		fmt.Printf("Message %s is younger than 10 minutes, ignoring it\n", msg.ID)
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getEnvVariables() (Variables, error) {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {
+		return Variables{}, errors.New("PROJECT_ID environment variable is not set")
+	}
+	subscriptionID := os.Getenv("SUBSCRIPTION_ID")
+	if subscriptionID == "" {
+		return Variables{}, errors.New("SUBSCRIPTION_ID environment variable is not set")
+	}
+
+	maxNackAttemptsStr := os.Getenv("MAX_NACK_ATTEMPTS")
+	maxNackAttempts, err := strconv.Atoi(maxNackAttemptsStr)
+	if err != nil {
+		maxNackAttempts = 5
+	}
+
+	messageAgeLimitStr := os.Getenv("MESSAGE_AGE_LIMIT")
+	messageAgeLimitMinutes, err := strconv.Atoi(messageAgeLimitStr)
+	if err != nil {
+		messageAgeLimitMinutes = 10
+	}
+	messageAgeLimit := time.Duration(messageAgeLimitMinutes) * time.Minute
+
+	return Variables{
+		ProjectID:       projectId,
+		SubscriptionID:  subscriptionID,
+		maxNackAttempts: maxNackAttempts,
+		messageAgeLimit: messageAgeLimit,
+	}, nil
+}
